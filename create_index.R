33c94dd1c67f531f3e4eadb477e58cc2bfdc1fd9
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2024-09-12 11:37:49 +0100

    Automatically generate pdf

--- create_index.R
+++ create_index.R
@@ -2,3 +2,6 @@ index <- file("index.html")
 writeLines(c(readLines("style.html"),readLines("README.md")), index)
 close(index)
 
+library(pagedown)
+chrome_print(input = "index.html", output = "cv.pdf")
+
